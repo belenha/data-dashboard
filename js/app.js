3e669cd9bc7848930d0037e0a4ad6767281fcaa6
belenha <lubinatica@gmail.com>
2018-06-01 18:10:08 -0500

    se suben estudiantes activas e inactivas con nombres

--- js/app.js
+++ js/app.js
@@ -10,32 +10,120 @@ console.log(morado);
 
 // Se pide un nombre de usuario y una contraseña para poder ingresar
 
-var firsStep = document.getElementById('send');
+// var firsStep = document.getElementById('send');
 
-function validar(){
-    var usname = document.getElementById('name').value;
-    var passw = document.getElementById('password').value;
+// function validar(){
+//     var usname = document.getElementById('name').value;
+//     var passw = document.getElementById('password').value;
 
-    if(usname.length != 0 && passw === 'laboratoria'){
-        // alert ('hola ' + usname);
-        var hide = document.getElementById('login');
-        hide.style.display = 'none';
+//     if(usname.length != 0 && passw === 'laboratoria'){
+//         // alert ('hola ' + usname);
+//         var hide = document.getElementById('login');
+//         hide.style.display = 'none';
 
-        // Se muestra el nombre que el usuario ingreso
-        var newName = document.createTextNode(usname);
-        var hiName = document.getElementById('helloName');
-        hiName.appendChild(newName);
-        hiName.style.display = 'block';
+//         // Se muestra el nombre que el usuario ingreso
+//         var newName = document.createTextNode(usname);
+//         var hiName = document.getElementById('helloName');
+//         hiName.appendChild(newName);
+//         hiName.style.display = 'block';
 
-        // se muestra el listado con las sedes
-        // var showlist = document.getElementById('sede');
-        // showlist.style.display = 'block';
+//         // se muestra el listado con las sedes
+//         // var showlist = document.getElementById('sede');
+//         // showlist.style.display = 'block';
 
+//     }
+//     else{
+//         alert('Ingresa tu nombre y contraseña :)');
+//     }
+// };
+
+// firsStep.addEventListener('click',validar);
+
+// *********** total estudiantes en generacion 1 de arequipa  ***********
+
+// var studentsAre = data.AQP['2016-2'].students.length;
+// console.log(studentsAre);
+
+// // var quantityAre = studentsAre.value;
+// var numberArequipa = document.createTextNode(studentsAre);
+
+// var showare = document.getElementById('areq');
+// showare.appendChild(numberArequipa);
+
+// ***********  estudiantes activas  ***********
+// var studentsAre = data.AQP['2016-2'].students;
+// console.log(studentsAre);
+
+
+var firstG = document.getElementById('firstGen');
+
+firstG.addEventListener('click', function(){
+var studentsAre = data.AQP['2016-2'].students;
+// console.log(studentsAre);
+var activeAre = 0;
+var inactiveAre = 0;
+var totalactive = [];
+var totalinactive = [];
+for(var i = 0; i < studentsAre.length; i++){
+    if(studentsAre[i]['active'] == true){
+        activeAre ++;
+        totalactive.push(studentsAre[i]['name']);
+        console.log(totalactive);
+        
+    } else {
+        inactiveAre ++;
+        totalinactive.push(studentsAre[i]['name']);
+        console.log(totalinactive);
+        // totalinactive.push(studentsAre[i]['active'] != true);
+        // console.log(totalinactive);
+        // var estuinactiva = studentsAre[i].name;
+        // console.log ( estuinactiva);
     }
-    else{
-        alert('Ingresa tu nombre y contraseña :)');
+}
+
+var container = document.getElementById('firstGen');
+var total = document.createElement('p');
+var text = document.createTextNode('Hay ' + activeAre + ' alumnas activas y ' + inactiveAre + ' alumnas inactivas');
+total.appendChild(text);
+container.appendChild(total);
+
+var namesAreActive = totalactive;
+var activeNames = document.createTextNode('Estudiantes activas '+ namesAreActive);
+var listActiveNames = document.createElement('p');
+listActiveNames.appendChild(activeNames);
+container.appendChild(listActiveNames);
+
+var namesAreInactive = totalinactive;
+var inactiveNames = document.createTextNode('Estudiantes inactivas '+ namesAreInactive);
+var listInactiveNames = document.createElement('p');
+listInactiveNames.appendChild(inactiveNames);
+container.appendChild(listInactiveNames);
+// total.id=('hide');
+total.style.display = 'block';
+});
+
+
+var secondG = document.getElementById('secondGen');
+
+secondG.addEventListener('click', function(){
+var studentsAreTwo = data.AQP['2017-1'].students;
+
+var activeAreTwo = 0;
+var inactiveAreTwo = 0;
+for(var j = 0; j < studentsAreTwo.length; j++){
+    if(studentsAreTwo[j]['active'] == true){
+        activeAreTwo ++;
+        // console.log(activeAre);
+    } else {
+        inactiveAreTwo ++;
+        // console.log('inactiveAre');
     }
-};
-
-firsStep.addEventListener('click',validar);
-
+}
+var containerTwo = document.getElementById('secondGen');
+var totalTwo = document.createElement('p');
+var textTwo = document.createTextNode('Hay ' + activeAreTwo + ' alumnas activas y ' + inactiveAreTwo + ' alumnas inactivas');
+totalTwo.appendChild(textTwo);
+containerTwo.appendChild(totalTwo);
+// total.id=('hide');
+totalTwo.style.display = 'block';
+});
---
+++ js/are.js
@@ -0,0 +1,22 @@
+// for(var i = 0; i = data.AQP['2016-2'].students.length; i++);
+
+var studentsAre = data.AQP['2016-2'].students.length;
+console.log(studentsAre);
+
+// var quantityAre = studentsAre.value;
+var numberArequipa = document.createTextNode(studentsAre);
+
+var showare = document.getElementById('areq');
+showare.appendChild(numberArequipa);
+
+// var active = data.AQP['2016-2'].students.active.length;
+
+for(var i = 0; i = data.AQP['2016-2'].students.active; i ++);
+if(active == true){
+    var showActiveAre 
+}
+
+
+// console.log(data.AQP['2016-2'].students.length);
+// console.log(data.AQP['2016-2'].students[0]);
+// console.log(data.AQP['2017-1'].students.length);
