a8987253e46c24319cfec7b777f5bc8a2f2e15a3
belenha <lubinatica@gmail.com>
2018-06-04 14:18:55 -0500

    se sube teacher y jedi de arequipa

--- js/are.js
+++ js/are.js
@@ -47,12 +47,25 @@ for(var i = 0; i < studentsAre.length; i++){
         totalinactive.push(studentsAre[i]['name']);
     }
 }
+var totalStudents = activeAre + inactiveAre;
+
+var activePercent = Math.round(activeAre * 100 / totalStudents);
+// console.log(activePercent);
+
+var inactivePercent = Math.round(inactiveAre * 100 / totalStudents);
+// console.log(inactivePercent);
+
 // ***** IMPRIMIENDO LAS ESTUDIANTES ACTIVAS, INACTIVAS DE LA PRIMERA GENERACION CON SUS NOMBRES
 var container = document.getElementById('firstGen');
 var total = document.createElement('p');
 var text = document.createTextNode('Hay ' + activeAre + ' estudiantes activas y ' + inactiveAre + ' estudiantes inactivas');
 total.appendChild(text);
 container.appendChild(total);
+
+var containerActive = document.createElement('p');
+var textActive = document.createTextNode('El porcentaje de alumnas activas es: ' + activePercent + '%');
+containerActive.appendChild(textActive);
+container.appendChild(containerActive);
 
 var namesAreActive = totalactive;
 console.log(namesAreActive);
@@ -61,19 +74,43 @@ var listActiveNames = document.createElement('p');
 listActiveNames.appendChild(activeNames);
 container.appendChild(listActiveNames);
 
+var containerInactive = document.createElement('p');
+var textInactive = document.createTextNode('El porcentaje de alumnas inactivas es: ' + inactivePercent + '%');
+containerInactive.appendChild(textInactive);
+container.appendChild(containerInactive);
+
 var namesAreInactive = totalinactive;
 var inactiveNames = document.createTextNode('Estudiantes inactivas: '+ namesAreInactive);
 var listInactiveNames = document.createElement('p');
 listInactiveNames.appendChild(inactiveNames);
 container.appendChild(listInactiveNames);
-// total.id=('hide');
-total.style.display = 'block';
+
+var proms = data.AQP['2016-2'].ratings;
+// console.log(proms);
+var promJedi = 0;
+var promTeacher = 0;
+
+for(var p = 0; p < proms.length; p++){
+     promJedi += (proms[p]['jedi'])/proms.length;
+     var jediTotal = promJedi.toFixed(1);
+    //  console.log(jediTotal);
+
+     promTeacher += (proms[p]['teacher'])/proms.length;
+     var teacherTotal = promTeacher.toFixed(1);
+    //  console.log(teacherTotal);
+  }
+     var jediText = document.createTextNode('El promedio del jedi es: ' + jediTotal);
+     var containerJedi = document.createElement('p');
+     var containerRatingJ = document.getElementById('firstGen');
+     containerJedi.appendChild(jediText);
+     containerRatingJ.appendChild(containerJedi);
+
+     var teacherText = document.createTextNode('El promedio del teacher es: ' + teacherTotal);
+     var containerTeacher = document.createElement('p');
+     var containerRatingT = document.getElementById('firstGen');
+     containerTeacher.appendChild(teacherText);
+     containerRatingT.appendChild(containerTeacher);
 });
-
-// var stuPoints = data.AQP['2016-2'].students[0].sprints[0].score.hse;
-// console.log(stuPoints);
-
-
 
 // ***** SEGUNDA GENERACION AREQUIPA******
 var secondG = document.getElementById('secondGen');
@@ -107,14 +144,24 @@ for(var h = 0; h < studentsAreTwo.length; h++){
         // console.log('inactiveAre');
     }
 }
+
+var totalStudentsTwo = activeAreTwo + inactiveAreTwo;
+
+var activePercentTwo = Math.round(activeAreTwo * 100 / totalStudentsTwo);
+
+var inactivePercentTwo = Math.round(inactiveAreTwo * 100 / totalStudentsTwo);
+
 // ***** IMPRIMIENDO LAS ESTUDIANTES ACTIVAS, INACTIVAS DE LA SEGUNDA GENERACION CON SUS NOMBRES
 var containerTwo = document.getElementById('secondGen');
 var totalTwo = document.createElement('p');
 var textTwo = document.createTextNode('Hay ' + activeAreTwo + ' estudiantes activas y ' + inactiveAreTwo + ' estudiantes inactivas');
 totalTwo.appendChild(textTwo);
 containerTwo.appendChild(totalTwo);
-// total.id=('hide');
-// totalTwo.style.display = 'block';
+
+var containerActiveTwo = document.createElement('p');
+var textActiveTwo = document.createTextNode('El porcentaje de alumnas activas es: ' + activePercentTwo + '%');
+containerActiveTwo.appendChild(textActiveTwo);
+containerTwo.appendChild(containerActiveTwo);
 
 var namesAreActive2 = totalactive2;
 console.log(namesAreActive2);
@@ -123,11 +170,40 @@ var listActiveNames2 = document.createElement('p');
 listActiveNames2.appendChild(activeNames2);
 containerTwo.appendChild(listActiveNames2);
 
+var containerInactiveTwo = document.createElement('p');
+var textInactiveTwo = document.createTextNode('El porcentaje de alumnas inactivas es: ' + inactivePercentTwo + '%');
+containerInactiveTwo.appendChild(textInactiveTwo);
+containerTwo.appendChild(containerInactiveTwo);
+
 var namesAreInactive2 = totalinactive2;
 var inactiveNames2 = document.createTextNode('Estudiantes inactivas: '+ namesAreInactive2);
 var listInactiveNames2 = document.createElement('p');
 listInactiveNames2.appendChild(inactiveNames2);
 containerTwo.appendChild(listInactiveNames2);
-// total.id=('hide');
-// total.style.display = 'block';
+
+var promsTwo = data.AQP['2017-1'].ratings;
+console.log(promsTwo);
+var promJedi2 = 0;
+var promTeacher2 = 0;
+
+for(var q = 0; q < promsTwo.length; q++){
+     promJedi2 += (promsTwo[q]['jedi'])/promsTwo.length;
+     var jediTotal2 = promJedi2.toFixed(1);
+     console.log(jediTotal2);
+
+     promTeacher2 += (promsTwo[q]['teacher'])/promsTwo.length;
+     var teacherTotal2 = promTeacher2.toFixed(1);
+     console.log(teacherTotal2);
+  }
+     var jediText2 = document.createTextNode('El promedio del jedi es: ' + jediTotal2);
+     var containerJedi2 = document.createElement('p');
+     var containerRatingJ2 = document.getElementById('secondGen');
+     containerJedi2.appendChild(jediText2);
+     containerRatingJ2.appendChild(containerJedi2);
+
+     var teacherText2 = document.createTextNode('El promedio del teacher es: ' + teacherTotal2);
+     var containerTeacher2 = document.createElement('p');
+     var containerRatingT2 = document.getElementById('secondGen');
+     containerTeacher2.appendChild(teacherText2);
+     containerRatingT2.appendChild(containerTeacher2);
 });
